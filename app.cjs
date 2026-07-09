2c4648d32e5c12a124b34c677eabb28e5c3bfd5d
Selja Sampe Rante <seljaworks@gmail.com>
2026-07-09 13:58:41 +0700

    fix: update entry point import path to point to the build directory

--- app.cjs
+++ app.cjs
@@ -1,4 +1,4 @@
 // Entry point for cPanel / LiteSpeed / Passenger to load ESM SvelteKit build
-import('./index.js').catch(err => {
+import('./build/index.js').catch(err => {
 	console.error('Failed to start SvelteKit app:', err);
 });
